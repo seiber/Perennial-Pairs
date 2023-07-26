574540c18e1930ae1d984acd916e11f2fab7d924
Dakota Seiber <Seiberdakota@gmail.com>
2023-07-26 18:44:37 -0500

    added fly.io to deploy app to cloud

--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@ import express from "express";
 import { plantSearch, plantStore } from "./controllers/plant.controller.js";
 
 const router = express.Router();
-
 router.get("/", plantSearch);
 router.post("/", plantStore);
 
