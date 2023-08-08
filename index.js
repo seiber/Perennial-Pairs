64a8bf8759641e60e169e34bf60f2d32c57ac79d
Dakota Seiber <Seiberdakota@gmail.com>
2023-08-08 17:51:16 -0500

    added additional css and refactored code

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import router from "./routes.js";
 import session from 'express-session';
 import cookieParser from "cookie-parser";
 
-
 const middleWareConnection = process.env.MIDDLEWARE_SECRET
 export const app = express();
 app.use(express.urlencoded({ extended: true })); //middleware for the server to handle multi-part-form-data on POST request and send it to the req.body
@@ -19,6 +18,8 @@ app.use(
       cookie: { secure: false }, 
     })
   );
+  //middle ware to add my CSS file to read from browser
+app.use(express.static('views'));
 app.set("view engine", "ejs");//using ejs template to render HTML
 app.use(router);//listening for acceptable client request URL from routes.js
 app.listen(3000,() =>{
